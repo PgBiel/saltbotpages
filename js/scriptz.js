66bee42c5b6d44afe0715a958a8ccc1a5838fc21
Un Known <jasonqan2004@gmail.com>
2017-01-31 21:06:13 -0500

    Limit 25 announcements

--- js/scriptz.js
+++ js/scriptz.js
@@ -36,6 +36,7 @@ window.getAnnouncements = function() {
         }, 5000);
       }
       if (ann) {
+        _.takeRight(ann, 25);
         var sh = new showdown.Converter({
           simplifiedAutoLink: true,
           excludeTrailingPunctuationFromURLs: true,
