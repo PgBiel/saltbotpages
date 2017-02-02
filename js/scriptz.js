35c80c133f84b0910a1aca6267bb2926a79bb5b2
Un Known <jasonqan2004@gmail.com>
2017-02-02 08:32:39 -0500

    LLLL can't satisfy a damn man

--- js/scriptz.js
+++ js/scriptz.js
@@ -53,7 +53,7 @@ window.getAnnouncements = function() {
           o.title.innerHTML = o.rawTitle;
           o.description = document.createElement("span");
           o.description.setAttribute("class", "announcement-description");
-          o.description.innerHTML = o.poster + " posted this " + moment(o.timestamp).fromNow() + moment(o.timestamp).format(" on LLLL");
+          o.description.innerHTML = o.poster + " posted this " + moment(o.timestamp).fromNow() + moment(o.timestamp).format(" on dddd, MMMM Do, YYYY at h mm a");
           o.title.appendChild(o.description);
           o.content = sh.makeHtml(o.content);
         });
